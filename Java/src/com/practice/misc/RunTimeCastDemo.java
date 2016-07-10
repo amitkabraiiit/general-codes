26d2c0e16f1ade66e81bfd8b076d569b7985179e
Amit Kabra <akabra@akabra-ltm.internal.salesforce.com>
2016-07-10 11:38:53 +0530

    syncing files from workspace

--- Java/src/com/practice/misc/RunTimeCastDemo.java
+++ Java/src/com/practice/misc/RunTimeCastDemo.java
@@ -18,20 +18,13 @@ public class RunTimeCastDemo {
 		X x = new X();
 		Y y = new Y();
 		Z z = new Z();
-		X xy = new Y(); // compiles ok (up the hierarchy, upcast)
-		X xz = new Z(); // compiles ok (up the hierarchy, upcast)
-		X x1 = y; // compiles ok (y is subclass of X)
-		X x2 = z; // compiles ok (z is subclass of X)
-		// Y y1 = new X();   
-			// compilation error as : 
-			// X is not a Y 
-			// Required Y found X
-		// Z z1 = new X();   compilation error same as above 
+		X xy = new Y(); // compiles ok (up the hierarchy, upcast, y is subclass of X)
+		X xz = new Z(); // compiles ok (up the hierarchy, upcast, z is subclass of X)
+		// Y y1 = new X();   // compilation error as :  X is not a Y , Required Y found X
 		// Y yz = new Z();   incompatible type (siblings)
 		// Y y1 = (Y) x; // compiles ok but produces runtime error ; ClassCastException: X cannot be cast to Y
-		// Z z1 = (Z) x; // compiles ok but produces runtime error ; ClassCastException: X cannot be cast to Z
-		Y y2 = (Y) x1; // compiles and runs ok (x1 is type Y) , downcast
-		Z z2 = (Z) x2; // compiles and runs ok (x2 is type Z) , downcast
+		Y y2 = (Y) xy; // compiles and runs ok (xy is type Y) , downcast
+		Z z2 = (Z) xz; // compiles and runs ok (xz is type Z) , downcast
 		// Z z3 = (Z) x; // compiles fine but runtime error ; ClassCastException: X cannot be cast to Z
 		// Y y3 = (Y) z;     inconvertible types (siblings)
 		// Z z3 = (Z) y;     inconvertible types (siblings)
