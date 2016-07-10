87e68d7e37cdf60aa5e1fddc9da05c9d76817004
Amit Kabra <akabra@akabra-ltm.internal.salesforce.com>
2016-07-10 11:38:53 +0530

    syncing files from workspace

--- Java/src/com/practice/sorting/InsertionSort.java
+++ Java/src/com/practice/sorting/InsertionSort.java
@@ -4,14 +4,16 @@ import java.util.Arrays;
 public class InsertionSort{
 
 	/*
-	 * Insertion sort maintains a sorted sub-array, and repetitively inserts new elements into it. The process is as following:
+	 * Insertion sort maintains a sorted sub-array, and repetitively inserts new elements into it. 
+	 * The process is as following:
 	 * Take the first element as a sorted sub-array.
 	 * Insert the second element into the sorted sub-array (shift elements if needed).
 	 * Insert the third element into the sorted sub-array.
 	 * Repeat until all elements are inserted. 
 	 * 
-	 * Suppose the array length is n. The outer loop runs roughly n times, and the inner loop on average runs n/2 times. 
-	 * The total time is about t(n) = n * (n/2) = O(n^2). In terms of the efficiency, this is the same as selection sort.
+	 * Suppose the array length is n. The outer loop runs roughly n times, and the inner 
+	 * loop on average runs n/2 times. The total time is about t(n) = n * (n/2) = O(n^2). 
+	 * In terms of the efficiency, this is the same as selection sort.
 	 */
 	public static void insertionSort(int[] ar)
 	{
@@ -19,8 +21,9 @@ public class InsertionSort{
 
 		int temp;
 		for (int i = 1; i < ar.length; i++) {
-			for(int j = i ; j > 0 ; j--){ // suppose till now sorted array is 1 2 7 9 and now we got 3 then we need to put 3 after 2, so 
-				// we first swap 3 with 9 then with 3 with 7 and then 3 gets its position.
+			for(int j = i ; j > 0 ; j--){ // suppose till now sorted array is 1 2 7 9 and now we got 
+			    // 3 then we need to put 3 after 2, so we first swap 3 with 9 then with 3 with 7 
+				// and then 3 gets its position.
 				if(ar[j] < ar[j-1]){
 					temp = ar[j];
 					ar[j] = ar[j-1];
