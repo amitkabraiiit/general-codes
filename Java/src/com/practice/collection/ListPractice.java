7f0ec713686e3e6dcf8d179e0d38ba24caf469c5
Amit Kabra <akabra@akabra-ltm.internal.salesforce.com>
2016-07-10 11:38:53 +0530

    syncing files from workspace

--- Java/src/com/practice/collection/ListPractice.java
+++ Java/src/com/practice/collection/ListPractice.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class ListPractice {
 
 	private void printList(){
-		List<Integer> l = new ArrayList<Integer>();
+		List<Integer> l = new ArrayList<>();
 		l.add(new Integer(0));
 		l.add(new Integer(1));
 		l.add(new Integer(2));	
@@ -21,16 +21,17 @@ public class ListPractice {
 		for(Integer i : l){
 			System.out.print(i+",");
 		}
-		
+		System.out.println();
 		for(int i=0 ; i<l.size();i++){ // similarly while loop
 			System.out.print(i+",");
 		}
-		
+		System.out.println();
 		Iterator<Integer> it = l.iterator();
 		while(it.hasNext()){
 			System.out.print(it.next()+",");
 		}
-		
+		System.out.println();
+		System.out.println(Arrays.toString(l.toArray()));
 	}
 
 	
