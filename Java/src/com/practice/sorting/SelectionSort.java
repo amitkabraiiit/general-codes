93c3827cc25790375fb2be758005569168cec9e1
Amit Kabra <akabra@akabra-ltm.internal.salesforce.com>
2016-07-10 11:38:53 +0530

    syncing files from workspace

--- Java/src/com/practice/sorting/SelectionSort.java
+++ Java/src/com/practice/sorting/SelectionSort.java
@@ -8,11 +8,11 @@ public class SelectionSort{
 	 * Find the next smallest element, and put it to the second position.
 	 * Repeat until all elements are in the right positions. 
 	 * 
-	 * The outer loop runs n-1 rounds, roughly one for each position. Inside each outer loop, the inner loop goes through the unsorted 
-	 * part of the array. On average, each inner loop scans through n/2 elements. 
-	 * The total time is roughly t(n) = (n Ð 1) * (n/2) *k = O(n^2), where k denotes the number of basic operations 
-	 * inside each inner loop; the constants are absorbed in the big-Oh notion. Note that in the big-Oh notion, 
-	 * we only care about the dominating factor (which is n^2 in this case).
+	 * The outer loop runs n-1 rounds, roughly one for each position. Inside each outer loop, the inner loop 
+	 * goes through the unsorted part of the array. On average, each inner loop scans through n/2 elements. 
+	 * The total time is roughly t(n) = (n - 1) * (n/2) *k = O(n^2), where k denotes the number of 
+	 * basic operations inside each inner loop; the constants are absorbed in the big-Oh notion. 
+	 * Note that in the big-Oh notion, we only care about the dominating factor (which is n^2 in this case).
 	 */
 	public static void selectionSort(int[] ar)
 	{
@@ -34,6 +34,5 @@ public class SelectionSort{
 	public static void  main(String args[]){
 		int [] arrayToBeSorted3 = {2,3,1,4,5,19,11,17,55,22,21,20,62};
 		selectionSort(arrayToBeSorted3);
-
 	}
 }
